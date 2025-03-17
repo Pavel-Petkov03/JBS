1671bca9c82e42310d935fec0dfa5cd54a378107
Pavel Petkov <elias5247698@abv.bg>
2025-03-17 19:38:22 +0200

    adding server endpoint

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import authRouter from "./routes/users"
+import bodyParser from "body-parser"
+import cookieParser  from "cookie-parser"
+const app = express(); 
+app.use(bodyParser.json());
+app.use(cookieParser ());
+app.use("/auth", authRouter);
+app.listen(8000, () => {
+    console.log("Listening");
+});
